6a9e9bd1eaa12111fa19626b6b2ac353a032a648
mostafa <mosio.fallahi@gmail.com>
2024-03-02 12:38:19 +0330

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,23 @@ module.exports = {
   theme: {
     extend: {
       container: {
-        padding : "1rem",
-        center: true
+        padding: "1rem",
+        center: true,
       },
       colors: {
         primary: {
-          DEFAULT : "#1D203A",
+          DEFAULT: "#1D203A",
           two: "#5774DC",
         },
         gray: {
-          30: "#1D203A4D"
+          30: "#1D203A4D",
         },
         orange: {
           first: "#FABB44",
-          last: "#F69522"
-        }
+          last: "#F69522",
+        },
       },
-      fontFamily: {
-        primary : "YekanBakh"
-      }
     },
   },
   plugins: [],
-}
+};
